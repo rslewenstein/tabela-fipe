06cc279d18892d880529b4ea4fac12c24b7a5bc7
rslewenstein <rafasoaresjf@gmail.com>
2020-11-18 21:20:15 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      <TabelaFipe/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TabelaFipe from './components/TabelaFipe';
+
+export default {
+  name: 'App',
+
+  components: {
+    TabelaFipe,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/TabelaFipe.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="main">
+        <h1>Tabela Fipe</h1>
+        <table border="1px">
+            <tr>
+                <td>Nome</td>
+                <td>Código</td>
+            </tr>
+            <tr v-for="item in list" v-bind:key="item.id">
+                <td>{{item.nome}}</td>
+                <td>{{item.codigo}}</td>
+            </tr>
+        </table>
+        <v-btn
+            small
+            color="primary"
+            dark
+        >
+            Checar
+        </v-btn>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+export default {
+    data(){
+        return {list:undefined}
+    },
+    mounted(){
+        Vue.axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+        .then((resp) => {
+            this.list=resp.data;
+            console.warn(resp.data)
+        })
+    }
+}
+</script>
+
+<style scoped>
+.main{
+    text-align: center;
+}
+</style>
